f14ad3acfc19a1d7908165857e019b478007c3d7
redfivewind <36482775+redfivewind@users.noreply.github.com>
2024-03-28 17:16:24 +0100

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -2,11 +2,11 @@
 #SECURITY: EDR, Disable shell history, ...
 #YAY
 
-# Start message
+# START MESSAGE
 echo "[!] ALERT: This script is potentially destructive. Use it on your own risk. Press any key to continue..."
 read
 
-# Initialise global variables
+# GLOBAL VARIABLES
 echo "[*] Initialising global variables..."
 AUDIO=""
 DESKTOP=""
@@ -29,7 +29,7 @@ USER_NAME="user"
 USER_PASS=""
 XEN=""
 
-# Select the target platform (BIOS or UEFI)
+# TARGET PLATFORM (BIOS/UEFI)
 echo "[*] Please select the target plaform ('bios' or 'uefi')..."
 read platform
 platform="${platform,,}"
@@ -47,7 +47,7 @@ else
     exit 1
 fi
 
-# Select the hypervisor (KVM or Xen)
+# HYPERVISOR (KVM/XEN)
 echo "[*] Please select the hypervisor ('kvm' or 'xen')..."
 read hypervisor
 hypervisor="${hypervisor,,}"
@@ -65,7 +65,7 @@ else
     exit 1
 fi
 
-# Enable/disable networking
+# ENABLE/DISABLE NETWORKING
 echo "[*] Enable networking? (yes/no)"
 read networking
 networking="${networking,,}"
@@ -83,7 +83,7 @@ else
     exit 1
 fi
 
-# Enable/disable (proprietary) GPU drivers
+# ENABLE/DISABLE (PROPRIETARY) GPU DRIVERS
 echo "[*] Enable AMD GPU drivers? (yes/no)"
 read gpu_amd
 gpu_amd="${gpu_amd,,}"
@@ -135,7 +135,7 @@ else
     exit 1
 fi
 
-# Enable/disable audio
+# ENABLE/DISABLE AUDIO
 echo "[*] Enable audio? (yes/no)"
 read audio
 audio="${audio,,}"
@@ -153,7 +153,7 @@ else
     exit 1
 fi
 
-# Select the desktop environment (optional)
+# SELECT THE DESKTOP ENVIRONMENT (OPTIONAL)
 echo "[*] Please select the desktop environment ('-', 'hyprland' or 'xfce')..."
 read desktop
 desktop="${desktop,,}"
@@ -175,7 +175,7 @@ else
     exit 1
 fi
 
-# Retrieve the LUKS password
+# LUKS PASSWORD
 echo "[*] Please enter the LUKS password: "
 read -s luks_pass_a
 echo "[*] Please reenter the LUKS password: "
@@ -188,7 +188,7 @@ else
     exit 1
 fi
 
-# Retrieve the user password
+# USER PASSWORD
 echo "[*] Please enter the user password: "
 read -s user_pass_a
 echo "[*] Please reenter the user password: "
@@ -201,7 +201,7 @@ else
     exit 1
 fi
 
-# Partition the disk
+# DISK PARTITIONING
 echo "[*] Partitioning the disk..."
 
 if [ "$UEFI" == 0 ];
@@ -243,14 +243,14 @@ done
 
 parted $DISK print
 
-# Setup LUKS
+# SETUP LUKS
 echo "[*] Setting up LUKS..."
 echo "[*] Formatting the second partition as LUKS crypto partition..."
 echo -n $LUKS_PASS | cryptsetup luksFormat $PART_LUKS --type luks1 -c twofish-xts-plain64 -h sha512 -s 512 --iter-time 10000 -
 echo -n $LUKS_PASS | cryptsetup luksOpen $PART_LUKS $LVM_LUKS -
 sleep 2
 
-# Setup LVM 
+# SETUP LVM
 echo "[*] Setting up LVM..."
 pvcreate /dev/mapper/$LVM_LUKS
 vgcreate $VG_LUKS /dev/mapper/$LVM_LUKS
@@ -258,7 +258,7 @@ lvcreate -L 6144M $VG_LUKS -n $LV_SWAP
 lvcreate -l 100%FREE $VG_LUKS -n $LV_ROOT
 sleep 2
 
-# Format & mount partitions
+# FORMAT & MOUNT PARTITIONS
 echo "[*] Formatting & mounting required partitions..."
 
 if [ "$UEFI" == 0 ];
@@ -284,30 +284,25 @@ mkswap /dev/mapper/$LVM_VG-$LV_SWAP -L $LV_SWAP
 swapon /dev/$LVM_VG/$LV_SWAP
 sleep 2
 
-# Update the pacman database
+# UPDATE THE PACMAN DATABASE
 echo "[*] Updating the pacman database..."
 pacman --disable-download-timeout --noconfirm -Scc
 pacman --disable-download-timeout --noconfirm -Syy
 
-# Bootstrap Arch Linux into /mnt
+# BOOTSTRAP SYSTEM
 echo "[*] Bootstrapping Arch Linux into /mnt including base packages..."
 pacstrap /mnt \
     amd-ucode \
     base \
-    dhcpcd \ #FIXME
     gptfdisk \
-    grub \ #FIXME
-    gvfs \ #FIXME
+    gvfs \ 
     intel-ucode \
     iptables-nft \ #FIXME
-    iwd \ #FIXME
     $KERNEL \
     linux-firmware \
     lvm2 \
     mkinitcpio \
     nano \
-    networkmanager \ #FIXME
-    net-tools \ #FIXME
     p7zip \
     rkhunter \ #FIXME
     sudo \
@@ -316,17 +311,31 @@ pacstrap /mnt \
     zip
 sleep 2
 
-# Networking
+# NETWORKING
 if [ "$NETWORKING" == 0 ];
 then
-    #FIXME
+    echo "[*] Skipping configuration for networking because networking is explicitely disabled."
 elif [ "$NETWORKING" == 1 ];
 then
-    #FIXME
+    echo "[*] Installing required packages for networking..."
+    chroot /mnt pacman --disable-download-timeout --needed --noconfirm -S dhcpcd \
+        iwd
+        network-manager \
+        net-tools
+
+    echo "[*] Configuring required services for networking..."
+    chroot /mnt systemctl enable dhcpcd
 else
     echo "[*] Variable 'NETWORKING' is '$NETWORKING' but must be 0 or 1. Exiting..."
     exit 1
 fi
+
+echo "[*] Setting up the hostname..."
+echo $HOSTNAME > /mnt/etc/hostname
+
+echo "[*] Populating '/etc/hosts'..."
+echo "127.0.0.1 localhost" > /mnt/etc/hosts
+echo "::1 localhost" >> /mnt/etc/hosts
 
 # GPU
 if [ "$GPU_AMD" == 0 ];
@@ -362,7 +371,7 @@ else
     exit 1
 fi
 
-# Audio
+# AUDIO
 if [ "$AUDIO" == 0 ];
 then
     #FIXME
@@ -374,7 +383,7 @@ else
     exit 1
 fi
 
-# Mount required filesystems
+# MOUNT REQUIRED FILESYSTEMS
 echo "[*] Mounting required filesystems..."
 mount -t proc proc /mnt/proc
 mount -t sysfs sys /mnt/sys
@@ -395,22 +404,18 @@ fi
 
 sleep 2
 
-# /etc/crypttab
+# /ETC/CRYPTTAB
 echo "[*] Adding the LUKS partition to /etc/crypttab..."
 printf "${LVM_LUKS}\tUUID=%s\tnone\tluks\n" "$(cryptsetup luksUUID $PART_LUKS)" | tee -a /mnt/etc/crypttab
 cat /mnt/etc/crypttab
 
-# fstab
+# /ETC/FSTAB
 echo "[*] Generating fstab file and setting the 'noatime' property..."
 genfstab -U /mnt > /mnt/etc/fstab
 sed -i 's/relatime/noatime/g' /mnt/etc/fstab
 sleep 2
 
-# mkinitcpio
-echo "[*] Adding the LUKS partition to /etc/crypttab..."
-printf "${LVM_LUKS}\tUUID=%s\tnone\tluks\n" "$(cryptsetup luksUUID $PART_LUKS)" | tee -a /mnt/etc/crypttab
-cat /mnt/etc/crypttab
-
+# MKINITCPIO
 echo "[*] Rebuilding initramfs image using mkinitcpio..."
 echo "MODULES=()" > /mnt/etc/mkinitcpio.conf
 echo "BINARIES=()" >> /mnt/etc/mkinitcpio.conf
@@ -419,18 +424,30 @@ arch-chroot /mnt /bin/bash -c "\
     mkinitcpio -p $KERNEL"
 sleep 2
 
-# Boot
+# BOOT
 echo "[*] Setting up the boot environment..."
-#FIXME
 
-# Keyboard layout
+if [ "$UEFI" == 0 ];
+then
+    echo "[*] ..."
+    #FIXME
+elif [ "$UEFI" == 1 ];
+then
+    echo "[*] ..."
+    #FIXME
+else
+    echo "[X] ERROR: Variable 'UEFI' is "$UEFI" but must be 0 or 1. Exiting..."
+    exit 1
+fi
+
+# KEYBOARD LAYOUT
 echo "[*] Loading German keyboard layout..."
 arch-chroot /mnt /bin/bash -c "\
     loadkeys de-latin1;\
     localectl set-keymap de"
 sleep 2
 
-# Setup locale
+# LOCALE
 echo "[*] Setting up the locale..."
 arch-chroot /mnt /bin/bash -c "\
     echo \"en_US.UTF-8 UTF-8\" > /etc/locale.gen;\
@@ -440,37 +457,23 @@ arch-chroot /mnt /bin/bash -c "\
     echo \"KEYMAP=de-latin1\" > /etc/vconsole.conf;\
     echo \"FONT=lat9w-16\" >> /etc/vconsole.conf"
 
-# Timezone & hardware clock
-echo "[*] Setting up the timezone & hardware clock..."
+# TIME
+echo "[*] Configuring the system time..."
 arch-chroot /mnt /bin/bash -c "\
     timedatectl set-timezone Europe/Berlin;\
     ln /usr/share/zoneinfo/Europe/Berlin /etc/localtime;\
-    hwclock --systohc --utc"
-sleep 2
-
-# Network time synchronisation
-echo "[*] Enabling network time synchronization..."
-arch-chroot /mnt /bin/bash -c "\
+    hwclock --systohc --utc;\
     timedatectl set-ntp true"
 sleep 2
 
-# Update system
+# SYSTEM UPDATE
 echo "[*] Updating the system..."
 arch-chroot /mnt /bin/bash -c "\
     pacman --disable-download-timeout --noconfirm -Scc;\
     pacman --disable-download-timeout --noconfirm -Syyu"
 sleep 2
 
-# Setup the hostname & /etc/hosts
-echo "[*] Setting up the hostname and /etc/hosts..."
-echo $HOSTNAME > /mnt/etc/hostname
-echo "127.0.0.1 localhost" > /mnt/etc/hosts
-echo "::1 localhost" >> /mnt/etc/hosts
-arch-chroot /mnt /bin/bash -c "\
-    systemctl enable dhcpcd"
-
-
-# User management
+# USER MANAGEMENT
 echo "[*] Adding the generic home user: '$USER_NAME'..."
 arch-chroot /mnt /bin/bash -c "\
     useradd -m -G wheel,users $USER_NAME"
@@ -486,7 +489,7 @@ sleep 2
 echo "[*] Disabling the root account..."
 passwd --root /mnt --lock root
 
-# Add user paths & scripts
+# USER PATHS & SCRIPTS
 echo "[*] Adding user paths & scripts..."
 mkdir -p /mnt/home/$USER_NAME/tools
 mkdir -p /mnt/home/$USER_NAME/workspace
@@ -501,11 +504,11 @@ echo "\
 arch-chroot /mnt /bin/bash -c "\
     chown -R $USER_NAME:users /home/$USER_NAME"
 
-# Unmount filesystems
+# UNMOUNT FILESYSTEMS
 echo "[*] Unmounting filesystems..."
 sync
 umount -a
 
-# Stop message
+# STOP MESSAGE
 echo "[*] Work done. Exiting..."
 exit 0
