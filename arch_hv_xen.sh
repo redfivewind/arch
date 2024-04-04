c158f4a9019563d8cfe2964d4fc62e8c27ab2adc
redfivewind <36482775+redfivewind@users.noreply.github.com>
2024-04-04 18:09:37 +0200

    .

--- arch_hv_xen.sh
+++ arch_hv_xen.sh
@@ -2,18 +2,23 @@ cp /boot/xen.efi /boot/efi/EFI/xen.efi
 
 objdump -h xen.efi | perl -ane '/\.pad/ && printf "0x%016x\n", hex($F[2]) + hex($F[3])'
 
-# Xen configuration
+# .config section
+#FIXME
 objcopy --add-section .config=xen.cfg --change-section-vma .config=0xffff82d041000000 /boot/efi/EFI/xen.efi /boot/efi/EFI/xen.efi
 
-# CPU microcode updates 
-objcopy --add-section .ucode=ucode.bin --change-section-vma .ucode=0xffff82d041010000 /boot/efi/EFI/xen.efi /boot/efi/EFI/xen.efi
-
-# XSM configuration
-objcopy --add-section .xsm=xsm.cfg --change-section-vma .xsm=0xffff82d041080000 /boot/efi/EFI/xen.efi /boot/efi/EFI/xen.efi
-
-# Kernel image
+# .kernel section
+#FIXME
 objcopy --add-section .kernel=vmlinux --change-section-vma .kernel=0xffff82d041100000 /boot/efi/EFI/xen.efi /boot/efi/EFI/xen.efi
 
-# Initial ramdisk
+# .ramdisk section
+#FIXME
 objcopy --add-section .ramdisk=initrd.img --change-section-vma .ramdisk=0xffff82d042000000 /boot/efi/EFI/xen.efi /boot/efi/EFI/xen.efi
+
+# .ucode section 
+#FIXME
+objcopy --add-section .ucode=ucode.bin --change-section-vma .ucode=0xffff82d041010000 /boot/efi/EFI/xen.efi /boot/efi/EFI/xen.efi
+
+# .xsm section
+#FIXME
+objcopy --add-section .xsm=xsm.cfg --change-section-vma .xsm=0xffff82d041080000 /boot/efi/EFI/xen.efi /boot/efi/EFI/xen.efi
 
