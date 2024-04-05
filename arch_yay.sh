8ce3b2497aae556ffc78b3caf74bed74ba9768b0
redfivewind <36482775+redfivewind@users.noreply.github.com>
2024-04-05 13:49:00 +0200

    Update arch_yay.sh

--- arch_yay.sh
+++ arch_yay.sh
@@ -1,32 +1,56 @@
+# Start message
+echo "[*] This script installs the YAY AUR helper on this Arch Linux system."
+echo "[!] ALERT: This script is potentially destructive. Use it on your own risk. Press any key to continue..."
+read
+
 # Global variables
+echo "[*] Initialising global variables..."
 TMP_PATH="/tmp/yay-bin/"
 USER_NAME=$(whoami)
 
 # Temporary install Git
+echo "[*] Installing package git..."
 sudo pacman --disable-download-timeout --needed --noconfirm -S git
 
 # Retrieve yay
+echo "[*] Cloning the 'yay-bin' repository from GitHub..."
 git clone https://aur.archlinux.org/yay-bin.git "$TMP_PATH"
 cd "$TMP_PATH"
 
 # Install yay
+echo "[*] Installing YAY AUR helper..."
 makepkg --noconfirm -si
 cd
 yay --version
 
 # Install required tools
-TOOLS="chkrootkit secure-delete"
-
-for Tool in $TOOLS; do
-    sudo pacman --disable-download-timeout --needed --noconfirm -S $Tool
-    yay --disable-download-timeout --needed --noconfirm -S $Tool
-done
-
-# Uninstall Git
-sudo pacman --noconfirm -Rns git
+echo "[*] Installing AUR base packages..."
+yay --disable-download-timeout --needed --noconfirm -S chkrootkit secure-delete
 
 # Cleanup
-srm -v -r $(readlink -f $0)
+echo "[*] Removing package git..."
+sudo pacman --noconfirm -Rns git
+
+echo "[*] Removing other packages that are no longer required..."
+sudo pacman --noconfirm -Rns $(pacman -Qdtq)
+
+echo "[*] Removing temporary files & directories..."
 srm -v -r "$TMP_PATH"
 
-sudo pacman --noconfirm -Rns $(pacman -Qdtq)
+echo "[*] Should this script be deleted? (yes/no)
+read delete_script
+
+if [ "$delete_script" == "yes ];
+then
+    echo "[*] Deleting the script..."
+    shred --force --remove=wipesync --verbose --zero $(readlink -f $0)
+elif [ "$delete_script" == "no" ];
+then
+    echo "[*] Skipping script deletion..."
+else
+    echo "[!] ALERT: Variable 'delete_script' is '$delete_script' but must be 'yes' or 'no'."
+fi
+
+# STOP MESSAGE
+echo "[*] Work done. Exiting..."
+exit 0
