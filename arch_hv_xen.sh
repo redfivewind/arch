5b5c84189dbb29b65276c255e917fdbdfb755190
redfivewind <36482775+redfivewind@users.noreply.github.com>
2024-04-05 14:22:44 +0200

    Update arch_hv_xen.sh

--- arch_hv_xen.sh
+++ arch_hv_xen.sh
@@ -2,7 +2,7 @@
 
 # Start message
 echo "[*] This script installs the Xen Hypervisor on Arch Linux."
-echo "[!] ALERT: This script is potentially destructive. Use it on your own risk. Press any key to continue..."
+echo "[!] ALERT: This script is potentially destructive. Use it on your own risk. Press any key to continue...x"
 read
 
 # Global variables
