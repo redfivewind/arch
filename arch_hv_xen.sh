3385cdd03335666369fbc5207a6289c4851bcb25
redfivewind <36482775+redfivewind@users.noreply.github.com>
2024-04-05 12:25:56 +0200

    Update arch_hv_xen.sh

--- arch_hv_xen.sh
+++ arch_hv_xen.sh
@@ -1,3 +1,5 @@
+#sudo objdump -h /boot/efi/xen.efi | perl -ane '/\.pad/ && printf "0x%016x\n", hex($F[2]) + hex($F[3])'
+
 # Start message
 echo "[*] This script installs the Xen Hypervisor on Arch Linux."
 echo "[!] ALERT: This script is potentially destructive. Use it on your own risk. Press any key to continue..."
@@ -6,45 +8,62 @@ read
 # Global variables
 echo "[*] Initialising global variables..."
 USER_NAME=$(whoami)
+XEN_EFI=/boot/xen.efi
 
-# Add user to libvirt group
-echo "[*] Adding user '$USER_NAME' to the libvirt group..."
-sudo usermod --append --groups libvirt $USER_NAME
+# Install required packages
+echo "[*] Install required packages..."
+yay --disable-download-timeout --needed --noconfirm --S libvirt-xen xen xen-qemu
+sudo pacman --disable-download-timeout --needed --noconfirm --S bridge-utils ebtables edk2-ovmf libguestfs libvirt seabios virt-manager virt-viewer
 
-# Configure libvirt
+# Enable libvirt user access
+echo "[*] Enabling libvirt access for user '$USER_NAME'..."
+
 echo "[*] Configuring libvirtd..."
 echo "unix_sock_group = \"libvirt\"" | sudo tee -a /etc/libvirt/libvirtd.conf
 echo "unix_sock_rw_perms = \"0770\"" | sudo tee -a /etc/libvirt/libvirtd.conf
 sudo systemctl enable libvirtd.service
 
+echo "[*] Adding user '$USER_NAME' to the libvirt group..."
+sudo usermod --append --groups libvirt $USER_NAME
+
 # Generate Xen UKI
 echo "[*] Generating a unified kernel image (UKI) of the Xen kernel..."
 sudo cp /boot/xen.efi /tmp/xen.efi
-#sudo objdump -h /boot/efi/xen.efi | perl -ane '/\.pad/ && printf "0x%016x\n", hex($F[2]) + hex($F[3])'
 
-echo "[*] Writing 'xen.cfg' to the new .config section..."
-CONFIG_PATH="/boot/xen.cfg"
-CONFIG_VMA=
-objcopy --add-section .config= --change-section-vma .config=0xffff82d041000000 /tmp/xen.efi /tmp/xen.efi
+PATH="/boot/xen.cfg"
+SECTION=".config"
+echo "[*] Writing '$CONFIG_PATH' to the new $SECTION section..."
+read -r -a OBJDUMP <<< $(objdump -h $XEN_EFI | grep .pad)
+VMA=$(printf "%X" $((((0x${OBJDUMP[2]} + 0x${OBJDUMP[3]} + 4096 - 1) / 4096) * 4096)))
+objcopy --add-section .config="PATH" --change-section-vma .config="$VMA" /tmp/xen.efi /tmp/xen.efi
 
-echo "[*] Writing 'xen.cfg' to the new .config section..."
-KERNEL_PATH=
-KERNEL_VMA=
-sudo objcopy --add-section .kernel=/boot/vmlinuz-linux-hardened --change-section-vma .kernel=0xffff82d041100000 /tmp/xen.efi /tmp/xen.efi
+PATH="/boot/initramfs-linux-hardened"
+SECTION=".initramfs"
+echo "[*] Writing '$CONFIG_PATH' to the new $SECTION section..."
+read -r -a OBJDUMP <<< $(objdump -h $XEN_EFI | grep .config)
+VMA=$(printf "%X" $((((0x${OBJDUMP[2]} + 0x${OBJDUMP[3]} + 4096 - 1) / 4096) * 4096)))
+objcopy --add-section .config="PATH" --change-section-vma .config="$VMA" /tmp/xen.efi /tmp/xen.efi
 
-echo "[*] Writing 'xen.cfg' to the new .config section..."
-RAMDISK_PATH=
-RAMDISK_VMA=
-sudo objcopy --add-section .ramdisk=/boot/initramfs-linux-hardened.img --change-section-vma .ramdisk=0xffff82d042000000 /tmp/xen.efi /tmp/xen.efi
+PATH="/boot/vmlinuz-linux-hardened"
+SECTION=".kernel"
+echo "[*] Writing '$CONFIG_PATH' to the new $SECTION section..."
+read -r -a OBJDUMP <<< $(objdump -h $XEN_EFI | grep .initramfs)
+VMA=$(printf "%X" $((((0x${OBJDUMP[2]} + 0x${OBJDUMP[3]} + 4096 - 1) / 4096) * 4096)))
+objcopy --add-section .config="PATH" --change-section-vma .config="$VMA" /tmp/xen.efi /tmp/xen.efi
 
-#echo "[*] Writing 'xen.cfg' to the new .config section..."
-#VMA_NEW=...
-#sudo objcopy --add-section .ucode=/boot/intel-ucode.bin --change-section-vma .ucode=0xffff82d041010000 /tmp/xen.efi /tmp/xen.efi
+PATH="/boot/xsm.cfg"
+SECTION=".xsm"
+echo "[*] Writing '$CONFIG_PATH' to the new $SECTION section..."
+read -r -a OBJDUMP <<< $(objdump -h $XEN_EFI | grep .kernel)
+VMA=$(printf "%X" $((((0x${OBJDUMP[2]} + 0x${OBJDUMP[3]} + 4096 - 1) / 4096) * 4096)))
+objcopy --add-section .config="PATH" --change-section-vma .config="$VMA" /tmp/xen.efi /tmp/xen.efi
 
-echo "[*] Writing '/boot/xsm.cfg' to the new .xsm section..."
-XSM_PATH=...
-XSM_VMA=
-sudo objcopy --add-section .xsm=/boot/xsm.cfg --change-section-vma .xsm=0xffff82d041080000 /tmp/xen.efi /tmp/xen.efi
+#PATH=
+#SECTION=".ucode"
+#echo "[*] Writing '$CONFIG_PATH' to the new $SECTION section..."
+#read -r -a OBJDUMP <<< $(objdump -h $XEN_EFI | grep .pad)
+#VMA=$(printf "%X" $((((0x${OBJDUMP[2]} + 0x${OBJDUMP[3]} + 4096 - 1) / 4096) * 4096)))
+#objcopy --add-section .config="PATH" --change-section-vma .config="$VMA" /tmp/xen.efi /tmp/xen.efi
 
 # Copy Xen UKI to EFI partition
 echo "[*] Copying the Xen UKI to the EFI partition..."
@@ -53,6 +72,10 @@ sudo cp /tmp/xen.efi /boot/efi/EFI/xen.efi
 # Sign Xen UKI using sbctl
 echo "[*] Signing the Xen UKI using sbctl..."
 sudo sbctl sign /boot/efi/EFI/xen.efi
+
+# Create a UEFI boot entry
+sudo efibootmgr --disk $DISK --part 1 --create --label 'xen' --load '\EFI\xen.efi' --unicode --verbose
+sudo efibootmgr -v
 
 # Clean up
 echo "[*] Cleaning up..."
