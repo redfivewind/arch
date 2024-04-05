727601664246cf31a7f544464e8d0af7776fc872
redfivewind <36482775+redfivewind@users.noreply.github.com>
2024-04-05 10:54:24 +0200

    Update arch_hv_xen.sh

--- arch_hv_xen.sh
+++ arch_hv_xen.sh
@@ -1,24 +1,34 @@
-cp /boot/xen.efi /boot/efi/EFI/xen.efi
+# Add user to libvirt group
+echo "[*] Adding user '$(whoami)' to the libvirt group..."
+sudo usermod --append --groups libvirt $(whoami)
 
-objdump -h xen.efi | perl -ane '/\.pad/ && printf "0x%016x\n", hex($F[2]) + hex($F[3])'
+# Configure libvirt
+echo "[*] Configuring libvirtd..."
+echo "unix_sock_group = \"libvirt\"" | sudo tee -a /mnt/etc/libvirt/libvirtd.conf
+echo "unix_sock_rw_perms = \"0770\"" | sudo tee -a /mnt/etc/libvirt/libvirtd.conf
+sudo systemctl enable libvirtd.service
 
-# .config section
-#FIXME
-objcopy --add-section .config=xen.cfg --change-section-vma .config=0xffff82d041000000 /boot/efi/EFI/xen.efi /boot/efi/EFI/xen.efi
+# Generate Xen UKI
+echo "[*] Generating a unified kernel image (UKI) of the Xen kernel..."
+sudo cp /boot/xen.efi /tmp/xen.efi
+sudo objdump -h /boot/efi/xen.efi | perl -ane '/\.pad/ && printf "0x%016x\n", hex($F[2]) + hex($F[3])'
 
-# .kernel section
-#FIXME
-objcopy --add-section .kernel=vmlinux --change-section-vma .kernel=0xffff82d041100000 /boot/efi/EFI/xen.efi /boot/efi/EFI/xen.efi
 
-# .ramdisk section
-#FIXME
-objcopy --add-section .ramdisk=initrd.img --change-section-vma .ramdisk=0xffff82d042000000 /boot/efi/EFI/xen.efi /boot/efi/EFI/xen.efi
+objcopy --add-section .config=xen.cfg --change-section-vma .config=0xffff82d041000000 /tmp/xen.efi /tmp/xen.efi
 
-# .ucode section 
-#FIXME
-objcopy --add-section .ucode=ucode.bin --change-section-vma .ucode=0xffff82d041010000 /boot/efi/EFI/xen.efi /boot/efi/EFI/xen.efi
+objcopy --add-section .kernel=vmlinux --change-section-vma .kernel=0xffff82d041100000 /tmp/xen.efi /tmp/xen.efi
 
-# .xsm section
-#FIXME
-objcopy --add-section .xsm=xsm.cfg --change-section-vma .xsm=0xffff82d041080000 /boot/efi/EFI/xen.efi /boot/efi/EFI/xen.efi
+objcopy --add-section .ramdisk=initrd.img --change-section-vma .ramdisk=0xffff82d042000000 /tmp/xen.efi /tmp/xen.efi
+
+objcopy --add-section .ucode=ucode.bin --change-section-vma .ucode=0xffff82d041010000 /tmp/xen.efi /tmp/xen.efi
+
+objcopy --add-section .xsm=xsm.cfg --change-section-vma .xsm=0xffff82d041080000 /tmp/xen.efi /tmp/xen.efi
+
+# Copy Xen UKI to EFI partition
+echo "[*] Copying the Xen UKI to the EFI partition..."
+sudo cp /tmp/xen.efi /boot/efi/EFI/xen.efi
+
+# Clean up
+echo "[*] Cleaning up..."
+rm -f -r /tmp/xen.efi
 
