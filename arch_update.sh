abc50ddf4e834800582cb1278d80a25be0faaeff
redfivewind <36482775+redfivewind@users.noreply.github.com>
2024-04-05 11:02:06 +0200

    Update arch_update.sh

--- arch_update.sh
+++ arch_update.sh
@@ -1,4 +1,7 @@
-# Update all packages" > /home/$USER_NAME/tools/update.sh
+# Start message
+echo "[*] This script updates all packages on this Arch Linux system."
+
+# Update all packages
 sudo pacman --disable-download-timeout --needed --noconfirm -Syyu
 yay --disable-download-timeout --needed --noconfirm -Syyu
 
@@ -7,3 +10,11 @@ sudo pacman --noconfirm -Rns $(pacman -Qdtq)
 
 # Fix the chrome-sandbox bug
 sudo chmod 4755 /opt/*/chrome-sandbox
+
+#Update the Xen UKI
+#FIXME: Rebuild Xen UKI
+#FIXME: Sign Xen UKI
+
+# Stop message
+echo "[*] Work done. Exiting..."
+exit 0
