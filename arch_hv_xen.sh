6a0cf057d7e414891f8276fe503a79d2ea50031c
redfivewind <36482775+redfivewind@users.noreply.github.com>
2024-06-28 16:26:42 +0200

    Update arch_hv_xen.sh

--- arch_hv_xen.sh
+++ arch_hv_xen.sh
@@ -5,7 +5,7 @@
 #FIXME: XSM FLASK
 
 # Start message
-echo "[*] This script installs the Xen virtualisation infrastructure on Arch Linux."
+echo "[*] This script installs the Xen virtualisation infrastructure on Arch Linux.x"
 echo "[!] ALERT: This script is potentially destructive. Use it on your own risk. Press any key to continue..."
 read
 
