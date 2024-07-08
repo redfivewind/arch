d2e8be410649b965d827be1a81bb9199a30ad4e0
redfivewind <36482775+redfivewind@users.noreply.github.com>
2024-07-08 12:54:40 +0200

    Update arch_update.sh

--- arch_update.sh
+++ arch_update.sh
@@ -2,13 +2,17 @@
 echo "[*] This script updates all packages on this Arch Linux system."
 
 # Update all packages
+echo "[*] Updating packages using pacman..."
 sudo pacman --disable-download-timeout --needed --noconfirm -Syyu
+echo "[*] Updating packages using yay..."
 yay --disable-download-timeout --needed --noconfirm -Syyu
 
 # Autoremove packages that are no longer required
+echo "[*] Removing packages that are no longer required..."
 sudo pacman --noconfirm -Rns $(pacman -Qdtq)
 
 # Fix the chrome-sandbox bug
+echo "[*] Applying the chrome-sandbox patch..."
 sudo chmod 4755 /opt/*/chrome-sandbox
 
 # Stop message
