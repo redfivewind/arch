919812510284adcb66e7913b8f81062951cb565c
redfivewind <36482775+redfivewind@users.noreply.github.com>
2024-07-08 13:25:05 +0200

    Update arch_core.sh

--- arch_core.sh
+++ arch_core.sh
@@ -1,5 +1,5 @@
 _01_00() {
-    echo "[*] --------- STAGE: 01 - PRE INSTALLATION ---------"
+    echo "[*] --------- STAGE: 01 - PRE INSTALLATION ---------x"
     _01_01_start_msg
     _01_02_init_global_vars
     _01_03_00_prompt_user
