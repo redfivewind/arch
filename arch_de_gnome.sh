0d1d9dafd2cb61c0b548c65925d0b907cc538e57
redfivewind <36482775+redfivewind@users.noreply.github.com>
2024-09-12 12:36:09 +0200

    Create arch_de_gnome.sh

---
+++ arch_de_gnome.sh
@@ -0,0 +1,46 @@
+# Start message
+echo "[*] This script installs GNOME on Arch Linux."
+echo "[!] ALERT: This script is potentially destructive. Use it on your own risk. Press any key to continue..."
+read
+
+# Global variables
+echo "[*] Initialising global variables..."
+
+# System update
+echo "[*] Updating the system..."
+sudo pacman --disable-download-timeout --needed --noconfirm -Syu
+yay --disable-download-timeout --needed --noconfirm -Syu
+
+# Install Wayland
+echo "[*] Installing Wayland..."
+sudo pacman --disable-download-timeout --needed --noconfirm -S egl-wayland libglvnd libinput wayland wayland-protocols
+
+# Install GNOME
+echo "[*] Installing GNOME..."
+sudo --disable-download-timeout --needed --noconfirm -S gdm gnome-control-center gnome-session gnome-shell gnome-terminal gnome-tweaks nautilus
+
+# Configure Hyprland
+echo "[*] Configuring GNOME..."
+#FIXME
+
+# Install further packages
+echo "[*] Installing further packages..."
+sudo --disable-download-timeout --needed --noconfirm -S \
+    brightnessctl \
+    greetd \
+
+# Configure services
+echo "[*] Configuring services..."
+sudo systemctl enable gdm
+
+# Cleanup
+echo "[*] Removing other packages that are no longer required..."
+sudo pacman --noconfirm -Rns $(pacman -Qdtq)
+
+echo "[*] Deleting this script"
+shred --force --remove=wipesync --verbose --zero $(readlink -f $0)
+
+# Stop message
+echo "[*] Installation of desktop environment XFCE is finished. You may reboot now."
+echo "[*] Work done. Exiting..."
+exit 0
