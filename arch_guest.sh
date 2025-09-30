689daee2a9f831ec860866a349572e8148729e7d
redfivewind <36482775+redfivewind@users.noreply.github.com>
2025-09-30 09:21:20 +0000

    Update arch_guest.sh

--- arch_guest.sh
+++ arch_guest.sh
@@ -1,5 +1,5 @@
 # Start message
-echo "[*] This script installs Arch Linux guest tools for virtualisation."
+echo "[*] This script installs Arch Linux guest packages for virtualisation."
 echo "[!] ALERT: This script is potentially destructive. Use it on your own risk. Press any key to continue..."
 read
 
@@ -8,6 +8,18 @@ echo "[*] Updating the system..."
 sudo pacman --disable-download-timeout --needed --noconfirm -Syu
 yay --disable-download-timeout --needed --noconfirm -Syu
 
-# Install guest tools
-echo "[*] Installing virtualisation guest tools..."
-sudo pacman --disable-download-timeout --needed --noconfirm -S qemu-guest-agent spice-vdagent
+# Install guest packages
+echo "[*] Installing virtualisation guest packages..."
+sudo pacman --disable-download-timeout --needed --noconfirm -S qemu-guest-agent spice-vdagent xf86-video-qxl
+
+# Cleanup
+echo "[*] Removing other packages that are no longer required..."
+sudo pacman --noconfirm -Rns $(pacman -Qdtq)
+
+echo "[*] Deleting this script..."
+shred --force --remove=wipesync --verbose --zero $(readlink -f $0)
+
+# Stop message
+echo "[*] Installation of the virtualisation guest packages is finished. You may reboot now."
+echo "[*] Work done. Exiting..."
+exit 0
