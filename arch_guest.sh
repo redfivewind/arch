94578fb40465a41d4de4d5083e1494d0c35bab55
redfivewind <36482775+redfivewind@users.noreply.github.com>
2025-09-30 09:19:12 +0000

    Update arch_guest.sh

--- arch_guest.sh
+++ arch_guest.sh
@@ -1,1 +1,9 @@
+# Start message
+echo "[*] This script installs Arch Linux guest tools for virtualisation."
+echo "[!] ALERT: This script is potentially destructive. Use it on your own risk. Press any key to continue..."
+read
 
+# System update
+echo "[*] Updating the system..."
+sudo pacman --disable-download-timeout --needed --noconfirm -Syu
+yay --disable-download-timeout --needed --noconfirm -Syu
